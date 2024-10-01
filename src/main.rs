6c09cd101592315cc696701374c6b8b24e0414d3
mifuyne <mifuyne@gmail.com>
2024-10-01 19:01:10 -0400

    🎉 Project init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Mqagical Macroquad Tutorial".to_owned(),
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(Color::from_hex(0x643c80));
+        next_frame().await
+    }
+}

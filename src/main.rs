eac9df8391f44d3ca168687690c90ffc79b74bfc
mifuyne <mifuyne@gmail.com>
2024-10-01 20:32:26 -0400

    Chapter 2 complete.

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,35 @@ fn window_conf() -> Conf {
     }
 }
 
+const SPEED: f32 = 5.0;
+
+
 #[macroquad::main(window_conf)]
 async fn main() {
+    // Get screen center
+    let mut x = screen_width() / 2.0;
+    let mut y = screen_height() / 2.0;
+
     loop {
         clear_background(Color::from_hex(0x643c80));
+
+        // Input setup
+        if is_key_down(KeyCode::Right) {
+            x += SPEED;
+        }
+        if is_key_down(KeyCode::Left) {
+            x -= SPEED;
+        }
+        if is_key_down(KeyCode::Down) {
+            y += SPEED;
+        }
+        if is_key_down(KeyCode::Up) {
+            y -= SPEED;
+        }
+
+        // Draw the circle
+        draw_circle(x, y, 16.0, Color::from_hex(0xb1de78));
+
         next_frame().await
     }
 }

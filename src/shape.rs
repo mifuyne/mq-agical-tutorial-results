32b77a41899d24e07d7858315820a380fbaaffbd
mifuyne <mifuyne@gmail.com>
2024-10-04 03:48:49 -0400

    Chapter 14 completed.

---
+++ src/shape.rs
@@ -0,0 +1,25 @@
+use macroquad::{color::Color, math::Rect};
+
+pub struct Shape {
+    pub size: f32,
+    pub speed: f32,
+    pub x: f32,
+    pub y: f32,
+    pub color: Color,
+    pub collided: bool,
+}
+    
+impl Shape {
+    pub fn collides_with(&self, other: &Self) -> bool {
+        self.rect().overlaps(&other.rect())
+    }
+
+    pub fn rect(&self) -> Rect {
+        Rect {
+            x: self.x - self.size / 2.0,
+            y: self.y - self.size / 2.0,
+            w: self.size,
+            h: self.size,
+        }
+    }
+}        
